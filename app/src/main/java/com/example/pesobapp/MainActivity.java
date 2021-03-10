d859b11428d1e69521386c45e124044c5b6a8259
BielCR <gabrielcr18@outlook.com>
2021-03-10 18:18:47 -0300

    Commit Inicial

---
+++ app/src/main/java/com/example/pesobapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pesobapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = "MainActivity";
+  private SectionsPageAdapter mSectionsPageAdapter;
+}
---
+++ app/src/main/java/com/example/pesobapp/SectionsPageAdapter.java
@@ -0,0 +1,26 @@
+package com.example.pesobapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class SectionsPageAdapter extends FragmentPagerAdapter {
+
+
+
+    public SectionsPageAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
